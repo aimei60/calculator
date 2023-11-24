31a7dc1237c55dc2eff16c3a0e23d209d1789a63
aimei60 <104590763+aimei60@users.noreply.github.com>
2023-11-24 07:32:48 +0000

    Added the simple calculation functions and the operate function for now

---
+++ myscript.js
@@ -0,0 +1,24 @@
+// calculator functions
+
+function addition(a,b) {
+    return a + b;
+}
+
+function subtraction(a,b) {
+    return a - b;
+}
+
+function multiplication(a,b) {
+    return a * b;
+}
+
+function division(a,b) {
+    return a / b;
+}
+
+
+function operate(a, b, operator) {
+    return operator(a,b);
+}
+
+console.log(operate(5, 2, division))
