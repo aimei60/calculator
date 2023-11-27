bb576af26ff59365456322937b1878dc73ebb2d6
aimei60 <104590763+aimei60@users.noreply.github.com>
2023-11-27 08:42:00 +0000

    I was able to get the calculator to display the numbers and decimal points when clicked

--- myscript.js
+++ myscript.js
@@ -12,7 +12,73 @@ const eight = document.querySelector(".eight");
 const nine = document.querySelector(".nine");
 const display = document.querySelector(".display")
 const clear = document.querySelector(".AC")
+const plus = document.querySelector(".plus")
+const minus = document.querySelector(".minus")
+const divide = document.querySelector(".divide")
+const multiply = document.querySelector(".multiply")
+const decimal = document.querySelector(".decimal")
+const equal = document.querySelector(".equals")
 
+decimal.addEventListener('click', function() {
+    decimalPoint = "."
+    display.append(decimalPoint)
+})
+
+
+zero.addEventListener('click', function() {
+    num = 0
+    display.append(num)
+
+}) 
+
+one.addEventListener('click', function() {
+    num = 1
+    display.append(num)
+})
+
+two.addEventListener('click', function() {
+    num = 2
+    display.append(num)
+})
+
+three.addEventListener('click', function() {
+    num = 3
+    display.append(num)
+})
+
+four.addEventListener('click', function() {
+    num = 4
+    display.append(num)
+})
+
+five.addEventListener('click', function() {
+    num = 5
+    display.append(num)
+})
+
+six.addEventListener('click', function() {
+    num = 6
+    display.append(num)
+})
+
+seven.addEventListener('click', function() {
+    num = 7
+    display.append(num)
+})
+
+eight.addEventListener('click', function() {
+    num = 8
+    display.append(num)
+})
+
+nine.addEventListener('click', function() {
+    num = 9
+    display.append(num)
+})
+
+clear.addEventListener('click', function(){
+    display.textContent = " "
+})
 
 function addition(a,b) {
     return a + b;
@@ -36,50 +102,11 @@ function operate(a, b, operator) {
 }
 
 
+equal.addEventListener('click', function(){
 
 
-zero.addEventListener('click', function() {
-    display.textContent = "0"
-}) 
+});
 
-one.addEventListener('click', function() {
-    display.textContent = "1"
-})
 
-two.addEventListener('click', function() {
-    display.textContent = "2"
-})
 
-three.addEventListener('click', function() {
-    display.textContent = "3"
-})
-
-four.addEventListener('click', function() {
-    display.textContent = "4"
-})
-
-five.addEventListener('click', function() {
-    display.textContent = "5"
-})
-
-six.addEventListener('click', function() {
-    display.textContent = "6"
-})
-
-seven.addEventListener('click', function() {
-    display.textContent = "7"
-})
-
-eight.addEventListener('click', function() {
-    display.textContent = "8"
-})
-
-nine.addEventListener('click', function() {
-    display.textContent = "9"
-})
-
-clear.addEventListener('click', function(){
-    display.textContent = " "
-})
-
-console.log(operate(4, 2, subtraction))
+console.log(operate(5, 2, subtraction))
